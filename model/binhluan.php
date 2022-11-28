b4306cde9b31cb07e7b85171f688f9c4536d5915
Manhnd10803 <118704220+Manhnd10803@users.noreply.github.com>
2022-11-28 22:22:13 +0700

    Chức năng bình luận sản phẩm

---
+++ model/binhluan.php
@@ -0,0 +1,14 @@
+<?php  
+    function add_comment($noi_dung, $ma_tk, $ngay_binh_luan, $ma_sp){
+        $sql = "INSERT INTO `commentary` (`ma_bl`, `noi_dung`, `ma_tk`, `ngay_bl`, `ma_sp`) VALUES (NULL, '{$noi_dung}', '{$ma_tk}', '{$ngay_binh_luan}', '{$ma_sp}')";
+        return pdo_execute($sql);
+    }
+    function show_comment($ma_sp){
+        $sql = "SELECT * FROM `commentary` WHERE `ma_sp` = '{$ma_sp}'";
+        return pdo_query($sql);
+    }
+    function check_binh_luan($ma_tk, $ma_sp){
+        $sql = "SELECT * FROM `cart` WHERE `ma_sp` = '{$ma_sp}' and `ma_tk` = '{$ma_tk}'";
+        return pdo_query_one($sql);
+    }
+?>
