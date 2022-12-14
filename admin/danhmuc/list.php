14c514b785095bc037d03b37aac368ab9c69ea96
trungdvph19910 <trungdvph19910@fpt.edu.vn>
2022-12-14 22:30:37 +0700

    sửa phần trang list bên admin

--- admin/danhmuc/list.php
+++ admin/danhmuc/list.php
@@ -2,7 +2,7 @@
     <h3 class="mb-50">Danh sách loại sản phẩm</h3>
     <table>
         <thead>
-            <th class="border-top-left"></th>
+            
             <th>MÃ LOẠI</th>
             <th>TÊN LOẠI</th>
             <!-- <th>ICON</th> -->
@@ -15,7 +15,7 @@
                 $xoadm = "index.php?act=xoadm&id=".$danhmuc['ma_loai'];
             ?>
             <tr>
-                <td><input type="checkbox"></td>
+                
                 <td><?php echo $danhmuc['ma_loai']?></td>
                 <td><?php echo $danhmuc['ten_loai']?></td>
                 <!-- <td><?php echo $danhmuc['icon']?></td> -->
@@ -26,8 +26,5 @@
             ?>
         </tbody>
     </table>
-    <button class="op">Chọn tất cả</button>
-    <button class="op">Bỏ chọn tất cả</button>
-    <button class="op">Xóa các mục đã chọn</button>
     <a href="index.php?act=adddm"><button class="op">Nhập thêm</button></a>
 </div>
